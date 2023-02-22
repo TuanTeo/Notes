a8fd1fca05e221a6325c630f32de1dba1da74e08
TuanTeo <feckerbg@gmail.com>
2023-02-22 22:37:58 +0700

    Giao diện đăng nhập đủ thành phần

--- app/App.js
+++ app/App.js
@@ -1,35 +1,8 @@
 import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import LoginScreenJs from "./screens/LoginScreen.js";
 
 export default App = () => {
     return (
-        <View style={styles.container}>
-            <TextInput
-                style={styles.text_input}
-                placeholder="Nhập tên Trang béo!"
-                // onChangeText={newText => setText(newText)}
-            />
-            <Text>Hello Trang Hấp</Text>
-            <TextInput
-                style={styles.text_input}
-                placeholder="Nhập mật khẩu Trang béo!"
-                // onChangeText={newText => setText(newText)}
-            />
-            <Text>Hello Trang Hấp</Text>
-        </View>
+        <LoginScreenJs />
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }, 
-    text_input: {
-        height: 40, 
-        width: '60%',
-        borderWidth: 2,
-        padding: 6
-    }
-});
---
+++ app/utils/logUtils.js
@@ -0,0 +1,7 @@
+function log(mess) {
+    console.log(mess);
+}
+
+export {
+    log
+}
