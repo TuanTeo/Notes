c3e31490ad5739f3a7b58990b735ebb7c93db37e
TuanTeo <feckerbg@gmail.com>
2023-04-27 22:19:50 +0700

    Thêm giao diện cài đặt

--- app/screens/settingscreen/SettingScreen.js
+++ app/screens/settingscreen/SettingScreen.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Switch, View} from 'react-native';
 import {Text} from 'react-native-paper';
 
 export default SettingScreen = ({navigation}) => {
+  const [enableBiometric, setEnableBiometric] = useState(false)
+
   return (
     <View style={styles.container}>
-      <Text>Settings</Text>
+      <View style={styles.itemContainer}>
+        <Text>Đăng nhập bằng sinh trắc học</Text>
+        <Switch value={enableBiometric} onValueChange={value => setEnableBiometric(value)}/>
+      </View>
     </View>
   );
 };
@@ -13,7 +18,12 @@ export default SettingScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 8,
-    rowGap: 8,
+    padding: 16,
+    rowGap: 16,
   },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
 });
